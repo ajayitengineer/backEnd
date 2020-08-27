8dc0667b2f9b11532773779b2eec5e2df94d5394
ajaybhatt <ajaybhattit@gmail.com>
2020-08-27 18:43:41 +0530

    backend

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Student;
+
+class StudentController extends Controller
+{
+    public function store(Request $request){
+        
+        $validatedData = $request->validate([
+        'first_name' => 'required',
+        'last_name' => 'required',
+        'email' => 'required',
+        'age' => 'required',
+        'password' => 'required',
+        'city' => 'required',
+        'state' => 'required',
+        'zip' => 'required',
+        'country' => 'required',
+        'pocket_money' => 'required'
+    ]);
+        
+    	$student = new Student();
+    	$student->first_name = $request->first_name;
+    	$student->last_name = $request->last_name;
+    	$student->email = $request->email;
+    	$student->password = bcrypt($request->password);
+    	$student->age = $request->age;
+    	$student->city = $request->city;
+    	$student->state = $request->state;
+    	$student->zip_code = $request->zip;
+    	$student->country = $request->country;
+    	$student->pocket_money = $request->pocket_money;
+
+    	if($student->save()){
+    		return response()->json([
+    		'message' => 'added successfully' 
+    	]);
+    	}else {
+    		return response()->json([
+              'message' => 'Internal server error'
+    		]);
+    	}
+    	
+
+
+    	
+
+    }
+
+    public function list(){
+        
+        $students = Student::get(['first_name','id','last_name','email','pocket_money','city','state','country','age','zip_code']);
+    	return response()->json([
+    		'payload' => $students,
+    		'message' => 'student list successfully fetched'
+    	]);
+    }
+
+    public function secondLargest(){
+
+    	$student = Student::OrderBy('pocket_money','desc')->offset(1)->limit(1)->get()->take(1);
+
+    	return response()->json([
+    		'payload' => $student,
+    		'message' => 'Student fetched successfully'
+    	]);
+    }
+}
---
+++ database/migrations/2020_08_26_191258_create_students_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->float('pocket_money',10,2);
+            $table->string('password');
+            $table->tinyInteger('age');
+            $table->string('city');
+            $table->string('state');
+            $table->bigInteger('zip_code');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
